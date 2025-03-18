491bf8fb0467eb995d30a6b13a22527cad95c806
Kāhu Newton <124933256+Smortwarzonefan@users.noreply.github.com>
2025-03-19 10:28:48 +1300

    Update C_02_Ingredient_Check.py
    Added appropriate comments to the file

--- C_02_Ingredient_Check.py
+++ C_02_Ingredient_Check.py
@@ -1,4 +1,5 @@
 # Functions Go Here
+# This function will ask the user for a food item, check if it's a valid salad ingredient, then return the value
 def ingredient_check(question, valid_ingredients=['cheese', 'lettuce', 'tomato', 'chicken',
 'pasta','rocket', 'onion', 'capsicum', 'spring onion', 'soy sauce', 'vinegar', 'salt', 'pepper'], num_letters = 3):
     """Checks that users enter the full word
@@ -8,6 +9,7 @@ def ingredient_check(question, valid_ingredients=['cheese', 'lettuce', 'tomato',
 
         response = input(question).lower()
 
+        # If the user inputs 'xxx', the loop will return 'xxx' which tells the code in the main function to stop the loop
         if response == 'xxx':
             return 'xxx'
 
@@ -24,6 +26,7 @@ def ingredient_check(question, valid_ingredients=['cheese', 'lettuce', 'tomato',
 # Main Routine Goes Here
 recipe = []
 
+# Uses the ingredient checker to ask the user for an ingredient and then adds it to a list
 while True:
     ingredient = ingredient_check("Please enter an ingredient: ")
     if ingredient == 'xxx':
